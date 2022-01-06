c4e7d3bc243ee8150bdffef027b6c3ac02dbc2e9
Madhav2133 <cs20b1047@iiitdm.ac.in>
2022-01-06 13:07:00 +0530

    msg

---
+++ msg_world/src/hello_world.cpp
@@ -0,0 +1,38 @@
+#include "ros/ros.h"
+#include "msg_world/hello_world.h"
+
+int main(int argc, char** argv)
+{
+    ros::init(argc,argv,"announce");
+
+    ros::NodeHandle nh;
+
+    ros::Publisher topic_publisher = nh.advertise<msg_world::hello_world>("Msg_To_World",1000);
+
+    msg_world::hello_world my_msg;
+
+    ros::Rate loop_rate(3);
+
+    my_msg.header.stamp = ros::Time::now();
+
+    my_msg.header.seq = 0;
+
+    my_msg.header.frame_id = "Hello World !";
+
+    my_msg.dm = "How you doin' ?";
+
+    while(ros::ok())
+    {
+        my_msg.header.seq++;
+
+        my_msg.header.stamp = ros::Time::now();
+
+        topic_publisher.publish(my_msg);
+
+        ros::spinOnce();
+
+        loop_rate.sleep();
+    }
+
+    return 0;
+}
